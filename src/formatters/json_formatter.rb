6cefb24ab6dc6831399528716add5d56be7242af
Minus <61775646+ShansheGundishvili@users.noreply.github.com>
2024-12-15 15:55:00 +0100

    feat: json formatter but for now it just does .tojson

--- src/formatters/json_formatter.rb
+++ src/formatters/json_formatter.rb
@@ -1,12 +1,7 @@
-class JSONFormater
+class JSONFormatter
   attr_accessor :destination_data_structure, :origin_data_structure
 
-  def initialize(origin_data_structure = {})
-    @origin_data_structure = origin_data_structure
-    @destination_data_structure = {}
-  end
-
   def format(data)
-    raise NotImplementedError, 'Method not implemented'
+    data.to_json
   end
 end
