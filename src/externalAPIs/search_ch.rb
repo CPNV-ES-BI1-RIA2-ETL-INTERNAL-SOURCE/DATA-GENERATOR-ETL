7f7f2a996eb3473dd6a5848a4c7ca8f3a3abc173
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-12-15 15:19:54 +0100

    refactor(#6): remove default query params to the complete class and add it the method only

--- src/externalAPIs/search_ch.rb
+++ src/externalAPIs/search_ch.rb
@@ -5,12 +5,12 @@ require 'json'
 class SearchAPI < ExternalAPI
   base_uri 'search.ch'
 
-  def initialize(options)
+  def initialize(options = {})
     super(options)
-    @options = { query: { show_tracks: "1", show_subsequent_stops: "1", time: '00:01' } }
   end
 
   def get_stationboard(options)
+    @options.merge!({ query: { show_tracks: "1", show_subsequent_stops: "1", time: '00:01' } })
     @options[:query].merge! options
     StationBoardResponse.new JSON.parse(self.class.get('/timetable/api/stationboard.json', @options).body, symbolize_names: true)
   end
