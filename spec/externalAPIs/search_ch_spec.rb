c10ce4ffba233bf1d9403f41f5840fdad7649ea8
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-02-28 15:14:22 +0100

    fix: add context to stationboard tests for clarity

--- spec/externalAPIs/search_ch_spec.rb
+++ spec/externalAPIs/search_ch_spec.rb
@@ -23,28 +23,30 @@ describe SearchAPI do
 
   describe '#stationboard' do
     # TODO missing context description
-    let(:station) { 'Yverdon-les-Bains' }
-    let(:date) { '12/12/2024' }
-    let(:response) { double('HTTParty::Response') }
+    context 'when fetching station timetable information' do
+      let(:station) { 'Yverdon-les-Bains' }
+      let(:date) { '12/12/2024' }
+      let(:response) { double('HTTParty::Response') }
 
-    before do
-      allow(SearchAPI).to receive(:get).and_return(response)
-    end
+      before do
+        allow(SearchAPI).to receive(:get).and_return(response)
+      end
 
-    it 'merges station and date into query parameters' do
-      api_client.get_stationboard(station, date)
-      options = api_client.instance_variable_get(:@options)
-      pp options
-      expect(options[:query]).to include(stop: station, date: date)
-    end
+      it 'merges station and date into query parameters' do
+        api_client.get_stationboard(station, date)
+        options = api_client.instance_variable_get(:@options)
+        pp options
+        expect(options[:query]).to include(stop: station, date: date)
+      end
 
-    it 'calls the API with correct endpoint and options' do
-      expect(SearchAPI).to receive(:get).with('/timetable/api/stationboard.json', hash_including(query: hash_including(stop: station, date: date)))
-      api_client.get_stationboard(station, date)
-    end
+      it 'calls the API with correct endpoint and options' do
+        expect(SearchAPI).to receive(:get).with('/timetable/api/stationboard.json', hash_including(query: hash_including(stop: station, date: date)))
+        api_client.get_stationboard(station, date)
+      end
 
-    it 'returns the API response' do
-      expect(api_client.get_stationboard(station, date)).to eq(response)
+      it 'returns the API response' do
+        expect(api_client.get_stationboard(station, date)).to eq(response)
+      end
     end
   end
 
