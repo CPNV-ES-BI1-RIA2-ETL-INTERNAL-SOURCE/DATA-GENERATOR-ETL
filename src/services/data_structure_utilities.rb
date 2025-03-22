63991dd07b3a1915ca70d0e5b54f0818afd8849c
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-03-22 13:53:18 +0100

    refactor(services): update service classes to use new DI container

--- src/services/data_structure_utilities.rb
+++ src/services/data_structure_utilities.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# DataStructureUtilities class for manipulating and transforming data structures
 class DataStructureUtilities
   def self.remap_json(data, origin_structure, destination_structure)
     raise NotImplementedError, 'Method not implemented'
