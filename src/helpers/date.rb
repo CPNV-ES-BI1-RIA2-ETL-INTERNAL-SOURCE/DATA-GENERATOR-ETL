55c8a69eda77bd87c1ef4ffd225eebe049b115e7
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-03-22 13:53:46 +0100

    refactor(server): adapt server and config to work with new DI system

--- src/helpers/date.rb
+++ src/helpers/date.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+# DateFormatters module providing date formatting utilities
 module DateFormatters
-  def self.get_month(i)
-    %w[janvier février mars avril mai juin juillet août septembre octobre novembre décembre].at(i.to_i - 1)
+  def self.get_month(month_number)
+    month_names = %w[January February March April May June July August September October November December]
+    month_names[month_number.to_i - 1]
   end
 end
