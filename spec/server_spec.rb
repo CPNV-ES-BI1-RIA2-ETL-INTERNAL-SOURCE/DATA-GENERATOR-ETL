a851982269ee883b2617add2788d6e93b0b30221
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-03-22 13:54:03 +0100

    test: update test files to work with new dependency injection

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -18,44 +18,63 @@ RSpec.describe Server, type: :request do
   def app
     Server
   end
+end
 
-  describe 'GET /api/stationboards/:region/:station' do
-    context 'given a valid region and station' do
-      it 'returns a 200 status and data in JSON format' do
-        get "/api/v1/stationboards/#{region}/#{station}", {}, 'HTTP_ACCEPT' => valid_mimetype
+RSpec.describe Server, 'API responses with valid inputs', type: :request do
+  include Rack::Test::Methods
+  
+  let(:station) { 'lausanne' }
+  let(:region) { 'CH' }
+  let(:date) { '2024-12-15' }
+  let(:valid_mimetype) { 'application/json' }
+  
+  def app
+    Server
+  end
+  
+  it 'returns a 200 status and data in JSON format' do
+    get "/api/v1/stationboards/#{region}/#{station}", {}, 'HTTP_ACCEPT' => valid_mimetype
 
-        expect(last_response.status).to eq(200)
-        expect(last_response.headers['Content-Type']).to include(valid_mimetype)
-      end
+    expect(last_response.status).to eq(200)
+    expect(last_response.headers['Content-Type']).to include(valid_mimetype)
+  end
+  
+  it 'returns a 200 status when including a valid date parameter' do
+    get "/api/v1/stationboards/#{region}/#{station}?date=#{date}", {}, 'HTTP_ACCEPT' => valid_mimetype
 
-      it 'returns a 415 status when the mimetype is unsupported' do
-        get "/api/v1/stationboards/#{region}/#{station}", {}, 'HTTP_ACCEPT' => invalid_mimetype
+    expect(last_response.status).to eq(200)
+    expect(last_response.headers['Content-Type']).to include(valid_mimetype)
+  end
+end
 
-        expect(last_response.status).to eq(415)
-      end
+RSpec.describe Server, 'API responses with invalid inputs', type: :request do
+  include Rack::Test::Methods
+  
+  let(:station) { 'lausanne' }
+  let(:region) { 'CH' }
+  let(:date) { '2024-12-15' }
+  let(:valid_mimetype) { 'application/json' }
+  let(:invalid_mimetype) { 'application/xml' }
+  
+  def app
+    Server
+  end
+  
+  it 'returns a 415 status when the mimetype is unsupported' do
+    get "/api/v1/stationboards/#{region}/#{station}", {}, 'HTTP_ACCEPT' => invalid_mimetype
 
-      it 'returns a 404 status when the station or region is invalid' do
-        get '/api/v1/stationboards/invalid_region/invalid_station', {}, 'HTTP_ACCEPT' => valid_mimetype
+    expect(last_response.status).to eq(415)
+  end
 
-        expect(last_response.status).to eq(404)
-      end
-    end
+  it 'returns a 404 status when the station or region is invalid' do
+    get '/api/v1/stationboards/invalid_region/invalid_station', {}, 'HTTP_ACCEPT' => valid_mimetype
 
-    context 'given a valid region, station, and date' do
-      it 'returns a 200 status and data in JSON format' do
-        get "/api/v1/stationboards/#{region}/#{station}?date=#{date}", {}, 'HTTP_ACCEPT' => valid_mimetype
+    expect(last_response.status).to eq(404)
+  end
+  
+  it 'returns a 404 status when the region, station, and date are invalid' do
+    get "/api/v1/stationboards/invalid_region/invalid_station?date=#{date}", {}, 'HTTP_ACCEPT' => valid_mimetype
 
-        expect(last_response.status).to eq(200)
-        expect(last_response.headers['Content-Type']).to include(valid_mimetype)
-      end
-    end
-
-    context 'given an invalid region, station, or date' do
-      it 'returns a 404 status' do
-        get "/api/v1/stationboards/invalid_region/invalid_station?date=#{date}", {}, 'HTTP_ACCEPT' => valid_mimetype
-
-        expect(last_response.status).to eq(404)
-      end
-    end
+    expect(last_response.status).to eq(404)
   end
 end
